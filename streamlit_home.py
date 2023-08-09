6438a92689586bc67ec547e2b306272906d76c82
AmitGupta2484 <129174892+AmitGupta2484@users.noreply.github.com>
2023-08-09 17:49:50 +0530

    resume_classification

--- streamlit_home.py
+++ streamlit_home.py
@@ -10,7 +10,7 @@ import streamlit as st
 import base64
 #import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
+#from tensorflow import keras
 from glob import glob
 import numpy as np
 import pandas as pd
