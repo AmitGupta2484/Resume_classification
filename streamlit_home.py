47363e66d201bb65603940aae20a1afb873f608c
AmitGupta2484 <129174892+AmitGupta2484@users.noreply.github.com>
2023-08-09 18:10:17 +0530

    resume_classification

--- streamlit_home.py
+++ streamlit_home.py
@@ -15,7 +15,7 @@ from glob import glob
 import numpy as np
 import pandas as pd
 import docx2txt
-from win32com import client
+#from win32com import client
 import os
 from docx import Document 
 import PyPDF2
